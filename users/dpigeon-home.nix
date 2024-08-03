0eac00e2e051f8efdc82fb5b8c7c3918dcd8aaa2
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2024-08-03 20:23:09 +0200

    Added git abbreviations for user dpigeon
    - ga: git add .
    - gc: git commit
    - gl: git log --graph (+format options)
    - gll: git log --graph (+format options, more info)

--- users/dpigeon-home.nix
+++ users/dpigeon-home.nix
@@ -30,6 +30,10 @@
     syntaxHighlighting.enable = true;
     shellAliases = {
       ll = "ls -lah";
+      ga = "git add .";
+      gc = "git commit";
+      gl = "git log --graph --pretty=format:'%Cred%h%C(yellow)%d%Creset - %Cblue%an%Creset - %Cgreen%s'";
+      gll = "git log --graph --pretty=format:'%Cred%H%C(yellow)%d%Creset - %Cblue%an%n%CgreenAuthor email: %ae%n%CgreenAuthor date: %ah%n%CgreenCommitter: %cn%n%CgreenCommitter email: %ce%n%CgreenCommit date: %ch%n%CgreenSigner: %GS%n%n%s%n%n%b'";
       nupdate = "sudo nixos-rebuild --flake .#DPigeon-MacOS switch";
     };
     history = {
