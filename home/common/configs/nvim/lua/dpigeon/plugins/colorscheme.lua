4e679f6670808aa7f980ad67c6d322914d2282e0
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2024-10-03 19:49:05 +0200

    added colorscheme stuff

--- home/common/configs/nvim/lua/dpigeon/plugins/colorscheme.lua
+++ home/common/configs/nvim/lua/dpigeon/plugins/colorscheme.lua
@@ -1,10 +1,39 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		init = function()
-			vim.cmd.colorscheme("tokyonight-night")
-			vim.cmd.hi("Comment gui=none")
-		end,
-	},
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+    opts = {
+      palettes = { duskfox = { bg1 = '#27253e' } },
+    },
+    init = function()
+      vim.cmd.colorscheme 'duskfox'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'tokyonight-storm'
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'catppuccin-macchiato'
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000,
+    -- init = function()
+    --   vim.cmd.colorscheme 'rose-pine-moon'
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
+  },
 }
