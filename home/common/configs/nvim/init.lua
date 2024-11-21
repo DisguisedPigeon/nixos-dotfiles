b3dd1b8e3b773e48cc6cedf45aec1293318774ad
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2024-11-21 17:01:23 +0100

    updated flake

--- home/common/configs/nvim/init.lua
+++ home/common/configs/nvim/init.lua
@@ -5,7 +5,7 @@ require "dpigeon.plugins"
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
+  callback = function() vim.hl.on_yank() end,
 })
 
 require("lazy").setup("dpigeon.plugins", {
