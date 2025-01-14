f28928a0074e4df1a9c07f5f2e0cc3535f9bd822
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-01-14 21:08:57 +0100

    changed harpoon and updated flake

--- home/common/configs/nvim/lua/config/plugins/harpoon.lua
+++ home/common/configs/nvim/lua/config/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
     local harpoon = require "harpoon"
 
@@ -18,5 +18,24 @@ return {
     vim.keymap.set("n", "<A-k>", function() harpoon:list():select(3) end)
 
     vim.keymap.set("n", "<A-l>", function() harpoon:list():select(4) end)
+
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
+
+    vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    local conf = require("telescope.config").values
   end,
 }
