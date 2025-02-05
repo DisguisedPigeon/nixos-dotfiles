d688bcfb972518c6ba5bfc0051cfa00defba6aa9
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-02-05 23:27:29 +0100

    fupdated. markview as well

--- home/common/configs/nvim/lua/config/plugins/lsp.lua
+++ home/common/configs/nvim/lua/config/plugins/lsp.lua
@@ -6,6 +6,7 @@ local servers = {
       },
     },
   },
+  ocamllsp = {},
   gleam = {},
   nixd = {},
   clangd = {},
--- home/common/configs/nvim/lua/config/plugins/markview.lua
+++ home/common/configs/nvim/lua/config/plugins/markview.lua
@@ -2,13 +2,15 @@ return {
   "OXY2DEV/markview.nvim",
   config = function()
     local presets = require "markview.presets"
+    local presets = require "markview.extras.checkboxes"
     require("markview").setup {
       latex = {
         enable = true,
       },
       checkboxes = presets.checkboxes.nerd,
-      headings = presets.headings.glow_center,
-      horizontal_rules = presets.horizontal_rules.thick,
+      headings = presets.headings.glow,
+      horizontal_rules = presets.horizontal_rules.arrowed,
+      tables = presets.tables.rounded,
     }
   end,
 }
