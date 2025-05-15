5983e3de0559c00572e91c0e41e45568b7a8943e
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-05-15 20:12:06 +0200

    forgot to enable lsps

--- home/common/configs/nvim/init.lua
+++ home/common/configs/nvim/init.lua
@@ -2,4 +2,16 @@ require "config.lazy"
 require "config.opts"
 require "config.keybinds"
 
+vim.lsp.enable("clangd")
+vim.lsp.enable("elixirls")
+vim.lsp.enable("gleam")
+vim.lsp.enable("html")
+vim.lsp.enable("ltex_plus")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("nil_ls")
+vim.lsp.enable("nixd")
+vim.lsp.enable("ocamllsp")
+vim.lsp.enable("ruff")
+vim.lsp.enable("rust_analyzer")
+
 require "config.autocommands"
