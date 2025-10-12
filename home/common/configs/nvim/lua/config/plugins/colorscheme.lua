cbd5df84c65d0bf85405ae8e2802215ecb33ca68
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-10-12 19:13:48 +0200

    formatted

--- home/common/configs/nvim/lua/config/plugins/colorscheme.lua
+++ home/common/configs/nvim/lua/config/plugins/colorscheme.lua
@@ -2,13 +2,12 @@ return {
   "rose-pine/neovim",
   name = "rose-pine",
   config = function()
-    require "rose-pine".setup {
+    require("rose-pine").setup {
       styles = {
         transparency = true,
       },
-
     }
 
     vim.cmd.colorscheme "rose-pine-moon"
-  end
+  end,
 }
