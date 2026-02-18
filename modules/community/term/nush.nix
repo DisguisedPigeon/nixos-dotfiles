a9c14c1484f524cd92f9166ff451e564f7a93319
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2026-02-18 01:56:07 +0100

    added completion to nu shell

--- modules/community/term/nush.nix
+++ modules/community/term/nush.nix
@@ -5,11 +5,19 @@
 
       # Launch nushell when running an interactive bash session.
       # This is done to inherit the environment variables.
-      programs.bash.initExtra = ''
-        if ! [ "$TERM" = "dumb" ] && [ -z "$BASH_EXECUTION_STRING" ]; then
-          exec nu
-        fi
-      '';
+      programs.bash = {
+        enable = true;
+        initExtra = ''
+          if ! [ "$TERM" = "dumb" ] && [ -z "$BASH_EXECUTION_STRING" ]; then
+            exec nu
+          fi
+        '';
+      };
+
+      programs.carapace = {
+        enable = true;
+        enableNushellIntegration = true;
+      };
 
       programs.nushell = {
         enable = true;
@@ -30,29 +38,94 @@
         };
 
         extraConfig = ''
-          # source starship.nu
-          # source zoxide.nu
-
+          # Prompt config - for starship -------------------------------------------------------------
           $env.PROMPT_INDICATOR = "❯ "
           $env.PROMPT_INDICATOR_VI_INSERT = "❯ "
           $env.PROMPT_INDICATOR_VI_NORMAL = "❮ "
           $env.PROMPT_MULTILINE_INDICATOR = "   :"
 
+
+          # Path extensions --------------------------------------------------------------------------
+          $env.PATH = $env.PATH
+            | split row (char esep)
+            | prepend /home/myuser/.apps
+            | append /usr/bin/env
+
+
+          # LS aliases -------------------------------------------------------------------------------
+          def lla [...args] { ls -la ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
+          def la  [...args] { ls -a  ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
+          def ll  [...args] { ls -l  ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
+          def l   [...args] { ls     ...(if $args == [] {["."]} else {$args}) | sort-by type name -i }
+
+
+          # Completion -------------------------------------------------------------------------------
+          let carapace_completer = {|spans: list<string>|
+            carapace $spans.0 nushell ...$spans
+            | from json
+            | ( if (
+                  $in
+                  | default []
+                  | where value == $"($spans | last)ERR"
+                  | is-empty
+                ) { $in }
+                else { null })
+          }
+          $env.CARAPACE_BRIDGES = 'zsh,bash,inshellisense'
+
+          let zoxide_completer = {|spans|
+              $spans
+              | skip 1
+              | zoxide query -l ...$in
+              | lines
+              | where {|x| $x != $env.PWD}
+          }
+
+          let multiple_completers = {|spans|
+            let expanded_alias = scope aliases
+              | where name == $spans.0
+              | get -o 0.expansion
+
+            let spans = if $expanded_alias != null {
+              $spans
+              | skip 1
+              | prepend ($expanded_alias | split row ' ' | take 1)
+            } else { $spans }
+
+            match $spans.0 {
+              __zoxide_z | __zoxide_zi => $zoxide_completer
+              _ => $carapace_completer
+            } | do $in $spans
+          }
+
+          $env.config = {
+            show_banner: false,
+            completions: {
+              case_sensitive: false
+              quick: true
+              partial: true
+              algorithm: "fuzzy"
+              external: {
+                enable: true
+                max_results: 100
+                completer: $multiple_completers
+              }
+            }
+          }
+
+
+          # NixOS/Home manager aliases ---------------------------------------------------------------
           def hupdate [username?: string, host?: string] {
-            home-manager --flake $".#(
-              $username
-              | default "${config.home.username}"
-            )-(
-              $host
-              | default "${config.home.sessionVariables.host}"
-            )" switch -b backup
+            let username: string = $username | default "${config.home.username}"
+            let host: string = $host | default "${config.home.sessionVariables.host}"
+
+            home-manager --flake $".#($username)-($host)" switch -b backup
           }
 
           def nupdate [host?: string] {
-            sudo nixos-rebuild --flake .#(
-              $host
-              | default "${config.home.sessionVariables.host}"
-            ) switch
+            let host: string = $host | default "${config.home.sessionVariables.host}"
+
+            sudo nixos-rebuild --flake .#($host) switch
           }
 
           def fupdate [username?: string, host?: string] {
