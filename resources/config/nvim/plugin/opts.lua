47a04952dfe9aa60b6d9230ae8cb2e4fbd567d00
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2026-05-03 20:22:48 +0200

    Some neovim settings

--- resources/config/nvim/plugin/opts.lua
+++ resources/config/nvim/plugin/opts.lua
@@ -2,6 +2,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 vim.o.signcolumn = "yes:1"
+vim.o.colorcolumn = "80"
 
 vim.o.foldlevel = 99
 
@@ -11,3 +12,14 @@ vim.o.ignorecase = true
 vim.o.scrolloff = 10
 
 vim.opt.formatoptions:remove("o")
+
+function FoldText()
+  return vim.fn.getline(vim.v.foldstart) -- First line of the fold
+    .. " ["
+    .. vim.v.foldend - vim.v.foldstart + 1 -- Size of the fold
+    .. "]"
+end
+
+vim.o.foldtext = "v:lua.FoldText()"
+vim.opt.fillchars:append({ fold = " " })
+vim.api.nvim_set_hl(0, "Folded", { link = "WinBar" })
