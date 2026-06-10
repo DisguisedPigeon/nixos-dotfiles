132ec36b735d3ea3100dc76b3fb9f39e6d285af0
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2026-06-10 19:48:23 +0200

    new keys idk how this works

--- modules/users/dpigeon/pepper.nix
+++ modules/users/dpigeon/pepper.nix
@@ -2,8 +2,7 @@
   flake.aspects.dpigeon-pepper.nixos = {
     users.users = {
       dpigeon.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKG5N/sJAGpJNlL3WRvNvRFMVMS/y+IChr8rTSnxzI9i root@salt"
-
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQPmyLMFnYp0Wb1NFJ2vrmlJ4hmMlzQGIZis4jLhxO9 root@salt"
       ];
       git.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF2usCLJudA5GvpM9UdDZJoxh+JUBeHcpWTVvyqHdTGn dpigeon@salt"
